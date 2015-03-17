0dcf78302e077928fc75f74c8868507224b0a4b9
bowee2010 <kefeiannan@163.com>
2015-03-17 17:23:16 +0800

    updated by Tony

--- src/main/java/com/tipmd/webapp/dao/impl/ScoreDaoImpl.java
+++ src/main/java/com/tipmd/webapp/dao/impl/ScoreDaoImpl.java
@@ -2,15 +2,19 @@ package com.tipmd.webapp.dao.impl;
 
 import java.util.List;
 
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import com.tipmd.webapp.dao.iface.IScoreDao;
 import com.tipmd.webapp.entity.Score;
 
 @Repository
-public class ScoreDaoImpl extends BaseDao implements IScoreDao
+public class ScoreDaoImpl implements IScoreDao
 {
 
+	@Autowired protected SqlSessionTemplate sqlSessionTemplate;  
+	
 	@Override
 	public void addScore(Score score) {
 		sqlSessionTemplate.insert("ScoreMapper.createScore", score);
